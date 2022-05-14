da79a692cf591107df7611706e14ac99d71fb404
Jerry Huang <jerry83314@gmail.com>
2022-05-14 22:57:54 +0800

    add: cors 套件

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
+var cors = require('cors');
 var logger = require('morgan');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
@@ -27,6 +28,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(cors());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
